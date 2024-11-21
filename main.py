b5b29dbbe6c8316953d8fa038a62d2269ccf09e3
Alexandr-sys2024 <time2016slot@gmail.com>
2024-11-21 18:37:59 +0300

    function for adding an order in mainpy

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 import  sqlite3
 
 def init_db():
-    conn = sqlite3.connect('business_order.db')
+    conn = sqlite3.connect('business_orders.db')
     cur = conn.cursor()
     cur.execute("""  
     CREATE TABLE IF NOT EXISTS orders (
@@ -15,6 +15,23 @@ def init_db():
     """)
     conn.commit()
     conn.close()
+
+def add_order():
+    conn = sqlite3.connect('business_orders.db')
+    cur = conn.cursor()
+
+    cur.execute(
+        "INSERT INTO orders (customer_name, order_details, status) VALUES (?, ?, 'Новый')",
+        (customer_name_entry.get(), order_details_entry.get()))
+
+    conn.commit()
+    conn.close()
+    customer_name_entry.delete(0, tk.END)
+    order_details_entry.delete(0, tk.END)
+
+#    view_orders()
+
+
 
 app = tk.Tk()
 app.title("Система управления заказами")
@@ -29,7 +46,7 @@ tk.Label(app, text="Детали заказа").pack()
 order_details_entry = tk.Entry(app)
 order_details_entry.pack()
 
-add_button = tk.Button(app, text="Добавить заказ")
+add_button = tk.Button(app, text="Добавить заказ", command=add_order)
 add_button.pack()
 
 columns = ("id", "customer_name", "order_details", "status")
